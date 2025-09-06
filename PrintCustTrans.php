71bc17469ee7a5d6c846a1f34a0b2a01cb5a9c3f
Juma Lungo <juma.lungo@zalongwa.co.tz>
2025-09-06 23:37:58 +0200

    updated PrintCustTrans page

--- PrintCustTrans.php
+++ PrintCustTrans.php
@@ -160,7 +160,7 @@ if (isset($PrintPDF)
 						ON debtorsmaster.currcode=currencies.currabrev
 						WHERE debtortrans.type=10
 						AND debtortrans.transno='" . $FromTransNo . "'";
-echo "line 163: ".$SQL;
+echo "line 163: ".$_SESSION['UserID'];
 exit;
 			if(isset($_POST['PrintEDI']) AND $_POST['PrintEDI']=='No') {
 				$SQL = $SQL . ' AND debtorsmaster.ediinvoices=0';
