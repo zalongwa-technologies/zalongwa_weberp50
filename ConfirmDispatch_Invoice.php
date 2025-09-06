8616a89919a382969da4ecb3d185c1cd4e4d7244
Juma Lungo <juma.lungo@zalongwa.co.tz>
2025-09-06 13:20:59 +0200

    updated confirmdispatch invoice

--- ConfirmDispatch_Invoice.php
+++ ConfirmDispatch_Invoice.php
@@ -148,7 +148,7 @@ if (!isset($_GET['OrderNumber']) and !isset($_SESSION['ProcessingOrder'])) {
 		$_SESSION['Items' . $identifier]->TaxGroup = $MyRow['taxgroupid'];
 		$_SESSION['Items' . $identifier]->DispatchTaxProvince = $MyRow['taxprovinceid'];
 
-		//$_SESSION['Items' . $identifier]->GetFreightTaxes(); (nimefuta)
+		$_SESSION['Items' . $identifier]->GetFreightTaxes();
 		$_SESSION['Items' . $identifier]->SpecialInstructions = $MyRow['specialinstructions'];
 
 		DB_free_result($GetOrdHdrResult);
