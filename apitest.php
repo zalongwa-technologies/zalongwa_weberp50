f19b8a05a01808e5ba6b70be6827d267a07a09e6
Juma Lungo <juma.lungo@zalongwa.co.tz>
2025-09-10 17:48:00 +0300

    updated apitest.php

---
+++ apitest.php
@@ -0,0 +1,10 @@
+<?php
+include ("phpxmlrpc/lib/xmlrpc.inc");
+$ServerURL = "https://zerp.saris.info.tz/api/api_xml-rpc.php";
+$Parameters["StockID"] = new xmlrpcval("ST001");
+$Parameters["Username"] = new xmlrpcval("leah");
+$Parameters["Password"] = new xmlrpcval("zalongwa");
+$Msg = new xmlrpcmsg(".xmlrpc_GetStockBalance", $Parameters);
+$Client = new xmlrpc_client($ServerURL);
+$Response = $Client->send($Msg);
+$Answer = php_xmlrpc_decode($Response->value());
